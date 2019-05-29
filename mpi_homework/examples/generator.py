c5ca70a1e8e85ea3f68f24c4cdaa4cd36f7e5b23
Tomasz Kępa <misterecco@gmail.com>
2019-05-29 22:54:18 +0200

    MPI: sparse matrix generator

---
+++ mpi_homework/examples/generator.py
@@ -0,0 +1,43 @@
+import argparse
+import random
+
+def print_list(l):
+    row = ""
+
+    for i in l:
+        row += f"{i} "
+
+    print(row[:-1])
+
+
+def generate_matrix(n, d):
+    values = []
+    col_indx = []
+    row_se = [0]
+
+    columns = list(range(n))
+
+    for _ in range(n):
+        cols = random.sample(columns, d)
+        cols.sort()
+
+        for col in cols:
+            col_indx.append(col)
+            values.append(random.random())
+
+        row_se.append(len(values))
+
+    print(f"{n} {n} {len(values)} {d}")
+    print_list(values)
+    print_list(row_se)
+    print_list(col_indx)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('n', help="Matrix dimension")
+    parser.add_argument('d', help="Number of nonzero elements in a row")
+
+    args = parser.parse_args()
+
+    generate_matrix(int(args.n), int(args.d))
